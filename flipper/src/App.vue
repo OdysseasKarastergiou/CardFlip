abd53012ac9d5411091bd42f2f3bd5e3996292a9
Odysseas Karastergiou <odysseasheat@hotmail.gr>
2021-10-29 16:26:30 +0300

    push

---
+++ flipper/src/App.vue
@@ -0,0 +1,124 @@
+<template>
+  <div id="app">
+    <header>
+      <h1>FlipCard App</h1>
+    </header>
+    <transition-group name="list" tag="Card">
+      <Card class="list-item" v-bind:flippedF="flippedF" @freebetEvent="offeredFreebet" @cancelEvent="freebetCanceled" v-for="card in cards" :key="card">
+        <template slot="front">
+          <div class="grid grid-cols-2">
+            <div>
+              <div class="flex text-purple-500 text-3xl font-bold">
+                <img src="https://pm.ua/content/uploads/Free_Bet_10_UAH_2_1_23c98cc95d.png" class="w-12">FREEBET OFFER</div>
+                <div class="  m-2 flex text-purple-500 text-xl font-semibold">one freebet (5+1) is waiting for you!</div>
+                <div class="bg-red-700 m-3 text-white flex  ">Exp.Date 15/05/2022</div>
+                </div>
+                <div class="m-10 text-purple-500 font-bold text-3xl">FREE <br />COLUMN</div>
+              </div>
+        </template>
+        <template slot="back">
+          <div class="divide-y-2 divide-black md:divide-solid">
+            <div>
+              <div class="m-6">
+                <span class="flex text-black text-2xl font-bold">Freebet with Quick Pick</span><br />
+              </div>
+            <div class="flex border-solid  border-4 border-gray-200 bg-gray-200">
+              <div class="inline-flex m-3 h-8 font-bold text-2xl text-white" v-for="(number,i) in numbersArrayF" :key="i">
+                <div class="rounded-full h-10 w-10 text-center" v-bind:class="{luckyN:i===5,unluckyN:i!==5}">{{ number }}</div>    </div><br />               
+              </div>
+              <div class="m-6">
+                <div class="text-xl flex font-bold">Consecutive draws:</div>
+                <div class="text-l flex">Click on "Submit" button to redeem your free bet and <br /> participate in the next draw with the above randomly <br /> selected numbers!</div></div></div>
+            </div>
+        </template>
+      </Card>
+    </transition-group>
+  </div>
+</template>
+
+<script>
+import Card from './components/Card.vue';
+
+export default {
+  name:"App",
+  components:{
+    Card
+  },
+  data(){
+    return{
+      flippedF:true,
+      flippedB:false,
+      numbersArrayF:[],
+      cards:5
+    }
+  },
+  methods:{
+    offeredFreebet(){
+      this.flippedF = true;
+      for(var i=0;i<6;i++){
+            let num = Math.floor(Math.random() * (40 - 1 + 1)) + 1;
+            for(let check of this.numbersArrayF){
+                while(check === num){
+                    num = Math.floor(Math.random() * (40 - 1 + 1)) + 1;
+                    }
+                }
+        this.numbersArrayF.push(num);
+      }
+
+    },
+    freebetCanceled(){
+      this.flippedB = false;
+      this.numbersArrayF.splice(0);
+    },
+     randomIndex: function () {
+      return Math.floor(Math.random() * this.items.length)
+    },
+    add: function () {
+      this.items.splice(this.randomIndex(), 0, this.nextNum++)
+    },
+    remove: function () {
+      this.items.splice(this.randomIndex(), 1)
+    },
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.luckyN{
+  --tw-text-opacity: 1;
+  color: rgba(59, 130, 246, var(--tw-text-opacity));
+  font-weight: 700;
+  --tw-bg-opacity: 1;
+  background-color: rgba(252, 211, 77, var(--tw-bg-opacity));
+}
+.unluckyN{
+  --tw-text-opacity: 1;
+  color: rgba(255, 255, 255, var(--tw-text-opacity));
+  --tw-bg-opacity: 1;
+  background-color: rgba(59, 130, 246, var(--tw-bg-opacity));
+}
+
+
+.board{
+  display: grid;
+  justify-content: center;
+}
+
+/* transitions */
+
+.list-item {
+  margin-right: 700px;
+  margin-left: 700px;
+}
+
+
+</style>
+
---
+++ flipper/src/components/Card.vue
@@ -0,0 +1,145 @@
+<template>
+  <div v-bind:class="flipped ? 'flip-container flipped': 'flip-container'">
+    <div class="flipper">
+      <div class="front border-solid border-2 overflow-auto bg-yellow-300 rounded">
+        <slot name="front"></slot>
+        <button class="frontFlipBtn bg-green-400 rounded text-center float-left m-2 text-white" 
+            v-on:click="freebetOffer">
+            FREE BET
+        </button>
+      </div>
+      <div class="back border-solid border-2 bg-white rounded">
+        <slot name="back"></slot>
+        <p class="backFlipBtn border-3 border-solid bg-white rounded-full absolute top-0 right-5"> Cancel</p>
+        <button class="backFlipBtn border-3 border-solid bg-white outline-black outline-solid rounded-full absolute top-0 right-1" 
+            v-on:click="cancelFreebet"> 
+            X
+        </button>
+        <button class="bg-blue-500 w-20 h-10 rounded text-white">FREE</button>
+        <button class="bg-green-400 w-20 h-10 rounded text-white">SUBMIT</button>
+      </div>
+    </div>
+  </div>
+
+</template>
+
+<script> 
+
+export default {
+  name: 'Card',
+  props:{
+    flippedF:{
+      type:Boolean
+    },
+    flippedB:{
+      type:Boolean
+    },
+    numbersArrayF:{
+      type:Array
+    }
+  },
+  data(){
+    return {
+        flipped:"",
+        numbersArray:[]
+    }
+  },
+  methods:{
+    freebetOffer(){
+      this.flipped = this.flippedF;
+      this.numbersArray = this.numbersArrayF;
+      this.$emit('freebetEvent');
+    },
+    cancelFreebet(){
+      this.flipped = this.flippedB;
+      this.numbersArray = this.numbersArrayF;
+      this.$emit('cancelEvent');
+    }
+
+  }
+};
+</script>
+
+<style type='text/css' scoped>
+i.frontFlipBtn,
+i.backFlipBtn {
+    position:absolute; 
+    right: 20px; 
+    top: 20px;
+    color:#FFFFFF;
+}
+i.backFlipBtn {
+    -webkit-transform: rotateY(-180deg);
+    -moz-transform: rotateY(-180deg);
+    -o-transform: rotateY(-180deg);
+    -ms-transform: rotateY(-180deg);
+    transform: rotateY(-180deg);
+}
+.flip-container {
+  -webkit-perspective: 1000;
+  -moz-perspective: 1000;
+  -o-perspective: 1000;
+  perspective: 1000;
+}
+.flip-container {
+  min-height: 120px;
+}
+.flipper {
+  -moz-transform: perspective(1000px);
+  -moz-transform-style: preserve-3d;
+  position: relative;
+}
+
+.front,
+.back {
+  -webkit-backface-visibility: hidden;
+  -moz-backface-visibility: hidden;
+  -o-backface-visibility: hidden;
+  backface-visibility: hidden;
+  -webkit-transition: 0.6s;
+  -webkit-transform-style: preserve-3d;
+  -moz-transition: 0.6s;
+  -moz-transform-style: preserve-3d;
+  -o-transition: 0.6s;
+  -o-transform-style: preserve-3d;
+  -ms-transition: 0.6s;
+  -ms-transform-style: preserve-3d;
+  transition: 0.6s;
+  transform-style: preserve-3d;
+  top: 0;
+  left: 0;
+  width: 100%;
+}
+.back {
+  -webkit-transform: rotateY(-180deg);
+  -moz-transform: rotateY(-180deg);
+  -o-transform: rotateY(-180deg);
+  -ms-transform: rotateY(-180deg);
+  transform: rotateY(-180deg);
+  position: absolute;
+
+}
+.flip-container.flipped .back {
+  -webkit-transform: rotateY(0deg);
+  -moz-transform: rotateY(0deg);
+  -o-transform: rotateY(0deg);
+  -ms-transform: rotateY(0deg);
+  transform: rotateY(0deg);
+}
+.flip-container.flipped .front {
+  -webkit-transform: rotateY(180deg);
+  -moz-transform: rotateY(180deg);
+  -o-transform: rotateY(180deg);
+  -ms-transform: rotateY(180deg);
+  transform: rotateY(180deg);
+  
+}
+.front {
+  z-index: 2;
+}
+
+
+  
+
+
+</style>
