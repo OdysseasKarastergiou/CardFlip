ee948ece816f3bd9a1c4cf36fbf0294a2bcfd11f
OdysseasKarastergiou <44501721+OdysseasKarastergiou@users.noreply.github.com>
2024-12-24 16:51:23 +0200

    Update main.js

--- flipper/src/main.js
+++ flipper/src/main.js
@@ -1,10 +1,8 @@
-import Vue from 'vue'
-import App from './App.vue'
-import "tailwindcss/tailwind.css"
+import Vue from 'https://cdn.jsdelivr.net/npm/vue@2.7.14/dist/vue.esm.browser.js';
+import App from './App.vue';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   render: h => h(App),
-}).$mount('#app')
-
+}).$mount('#app');
